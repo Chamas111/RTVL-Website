93111c6c82eddbf89faff611b8418bd66c0db0a9
Chamas111 <ahmad.chamas87@gmail.com>
2024-02-18 14:33:03 +0100

    create card, CarsItem, Footer, Services, Products, Signup componnents with cass files

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import "./App.css";
 import Navbar from "./Components/Navbar";
 import { Routes, Route } from "react-router-dom";
 import Home from "./Components/pages/Home";
+import SignUp from "./Components/pages/SignUp";
+import Products from "./Components/pages/Products";
+import Services from "./Components/pages/Services";
 
 function App() {
   return (
@@ -9,6 +12,9 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />}></Route>
+        <Route path="/services" element={<Services />}></Route>
+        <Route path="/products" element={<Products />}></Route>
+        <Route path="/sign-up" element={<SignUp />}></Route>
       </Routes>
     </>
   );
